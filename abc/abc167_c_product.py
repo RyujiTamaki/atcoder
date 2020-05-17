5240ad634998352d65308074b6c20164b5209d4e
RyujiTamaki <e44119rt@gmail.com>
2020-05-17 20:32:13 +0900

    add

--- abc/abc167_c_product.py
+++ abc/abc167_c_product.py
@@ -1,4 +1,5 @@
 from itertools import product
+import numpy as np
 
 N, M, X = map(int, input().split())
 
@@ -9,6 +10,16 @@ for i in range(N):
     C[i] = inp[0]
     A[i] = inp[1:]
 
+C = np.array(C)
+A = np.array(A)
+ans = C.sum() + 1
 
-min_val = sum(C)
-ans = False
+for choices in product((True, False), repeat=N):
+    choices = np.array(list(choices), dtype=bool)
+    if np.all(A[choices, :].sum(axis=0) >= X):
+        ans = min(ans, C[choices].sum())
+
+if ans == C.sum() + 1:
+    print(-1)
+else:
+    print(ans)
